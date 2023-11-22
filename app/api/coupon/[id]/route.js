ccdf11632c36f3967fb0a8db396e591d78088953
zariyatyasin <zariyatyasin789@gmail.com>
2023-11-23 01:29:38 +0600

    gu
    gu

--- app/api/coupon/[id]/route.js
+++ app/api/coupon/[id]/route.js
@@ -1,58 +1,60 @@
 import { NextResponse } from "next/server";
 
- 
 import db from "../../../../utils/db";
- 
+
 import Coupon from "../../../../model/Coupon";
 import Category from "../../../../model/Category";
-export const GET = async (request , { params } ) => {
- 
- 
-    try {
-     db.connectDb()
-     const { id } = params;
+export const GET = async (request, { params }) => {
+  try {
+    db.connectDb();
+    const { id } = params;
 
-     const coupon = await Coupon.findById(id).populate({ path: "applicableCategories", model: Category });
-       db.disconnectDb()
-      return NextResponse.json( {
-        coupon:coupon
-      } ,{
+    const coupon = await Coupon.findById(id).populate({
+      path: "applicableCategories",
+      model: Category,
+    });
+    db.disconnectDb();
+    return NextResponse.json(
+      {
+        coupon: coupon,
+      },
+      {
         status: 201,
-      })
-    } catch (err) {
-      return new NextResponse({message:err}, { status: 500 });
-    }
-  };
-  export const DELETE = async (request, { params }) => {
-    try {
-      db.connectDb();
-      const { id } = params;
-  
-      const deletedCoupon = await Coupon.findByIdAndRemove(id);
-  
-      if (!deletedCoupon) {
-        return NextResponse.json(
-          {
-            message: "Coupon not found",
-          },
-          {
-            status: 404,
-          }
-        );
       }
-  
-      db.disconnectDb();
+    );
+  } catch (err) {
+    return new NextResponse({ message: err }, { status: 500 });
+  }
+};
+export const DELETE = async (request, { params }) => {
+  try {
+    db.connectDb();
+    const { id } = params;
+
+    const deletedCoupon = await Coupon.findByIdAndRemove(id);
+
+    if (!deletedCoupon) {
       return NextResponse.json(
         {
-          message: "Coupon deleted successfully !",
-          coupon: await Coupon.find({}).sort({ updatedAt: -1 }),
+          message: "Coupon not found",
         },
         {
-          status: 200,
+          status: 404,
         }
       );
-    } catch (err) {
-      return NextResponse.json({ message: err.message }, { status: 500 });
     }
-  };
-  
+
+    db.disconnectDb();
+    return NextResponse.json(
+      {
+        message: "Coupon deleted successfully !",
+        coupon: await Coupon.find({}).sort({ updatedAt: -1 }),
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    return NextResponse.json({ message: err.message }, { status: 500 });
+  }
+};
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,87 +9,108 @@ import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalance
 import { redirect } from "next/navigation";import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
 import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
 import CardGiftcardOutlinedIcon from '@mui/icons-material/CardGiftcardOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
+import Order from "../../model/Order"
 async function getData({ params, searchParams }) {
   const session = await getCurrentUser();
   if (!session) {
     redirect("/signin");
   }
+
+  const  orders = await Order.find({ user: session?.id, status: "Processing" })
+  .sort({
+    createdAt: -1,
+  })
+  .lean();
+
+ 
   const tab = searchParams.tab || 0;
   return {
     session,
     tab,
+    orders
   };
 }
+
+
 export default async function page({ searchParams }) {
-  const { session, tab } = await getData({ searchParams });
+  const { session, tab,orders } = await getData({ searchParams });
  
   return (
-    <div className="  ">
-      <Header />
+<div className="">
+  <Header />
 
-      <Layout
-        data={{
-          ...session,
-          tab,
-        }}
-      >
-    
-        <div className=" flex gap-8 ">
-        <div className="py-10 px-4 bg-white  flex-1  ">
-          <h5 className="  font-semibold text-gray-950  text-xl">Hi, {session.name}</h5>
+  <Layout
+    data={{
+      ...session,
+      tab,
+    }}
+  >
+    <div className="flex flex-col sm:flex-row gap-8">
+      <div className="py-10 px-4 bg-white flex-1">
+        <h5 className="font-semibold text-gray-950 text-xl">Hi, {session.name}</h5>
 
-          <div className="flex justify-between mt-5">
-
-          <div className=" flex justify-center  items-center flex-col">
+        <div className="flex  flex-row flex-wrap gap-4 justify-between mt-5">
+          <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
             0
-
-            <p className=" text-sm mt-2 text-gray-500">Coupons</p>
-           </div>
-           <div className="flex justify-center  items-center flex-col">
-            <PaidOutlinedIcon sx={{fontSize:28}}/>
-            <p className=" text-sm mt-2 text-gray-500">Points</p>
-           </div>
-           <div className="flex justify-center  items-center flex-col">
-            <AccountBalanceWalletOutlinedIcon sx={{fontSize:28}}/>
-            <p className=" text-sm mt-2 text-gray-500">Wallet</p>
-           </div>
-           <div className="flex justify-center  items-center flex-col">
-            <InventoryOutlinedIcon sx={{fontSize:28}}/>
-            <p className=" text-sm mt-2 text-gray-500">Requested product</p>
-           </div>
-           <div className="flex justify-center  items-center flex-col">
-            <CardGiftcardOutlinedIcon sx={{fontSize:28}}/>
-            <p className=" text-sm mt-2 text-gray-500">Gift Card</p>
-           </div>
+            <p className="text-sm mt-2 text-gray-500">Coupons</p>
           </div>
+          <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
+            <PaidOutlinedIcon sx={{ fontSize: 28 }} />
+            <p className="text-sm mt-2 text-gray-500">Points</p>
+          </div>
+          <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
+            <AccountBalanceWalletOutlinedIcon sx={{ fontSize: 28 }} />
+            <p className="text-sm mt-2 text-gray-500">Wallet</p>
+          </div>
+          <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
+            <InventoryOutlinedIcon sx={{ fontSize: 28 }} />
+            <p className="text-sm mt-2 text-gray-500">Requested</p>
+          </div>
+          <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
+            <CardGiftcardOutlinedIcon sx={{ fontSize: 28 }} />
+            <p className="text-sm mt-2 text-gray-500">Gift Card</p>
+          </div>
+         
         </div>
-        <div className="py-10 px-4  bg-white flex justify-between w-64">
-           <div className=" flex justify-center  items-center flex-col">
-            <SmsOutlinedIcon sx={{fontSize:28}}/>
-
-            <p className="  text-sm mt-2 text-gray-500">My Message</p>
-           </div>
-           <div className="flex justify-center  items-center flex-col">
-            <FeedbackOutlinedIcon sx={{fontSize:28}}/>
-            <p className=" text-sm mt-2 text-gray-500">FeedBack</p>
-           </div>
+      </div>
+      <div className="py-10 px-4 bg-white flex justify-between w-full sm:w-64">
+        <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
+          <SmsOutlinedIcon sx={{ fontSize: 28 }} />
+          <p className="text-sm mt-2 text-gray-500">My Message</p>
         </div>
-
-
+        <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
+        <BusinessOutlinedIcon sx={{ fontSize: 28 }} />
+            <p className="text-sm mt-2 text-gray-500">Address</p>
         </div>
-        <div className="mt-5 py-10 px-4 bg-white">
-        <h5 className="  font-semibold text-gray-950  text-xl">My Orders</h5>
-
-        <div></div>
+        <div className="flex justify-center items-center flex-col mb-4 sm:mb-0">
+          <FeedbackOutlinedIcon sx={{ fontSize: 28 }} />
+          <p className="text-sm mt-2 text-gray-500">FeedBack</p>
         </div>
-      </Layout>
+      </div>
     </div>
+    <div className="mt-5 py-10 px-4 bg-white">
+      <h5 className="font-semibold text-gray-950 text-xl">My Orders</h5>
 
-    // <div>
+      <div className="py-8 flex items-center justify-center">
+        {orders.length !== 0 ? (
+          <div>some product</div>
+        ) : (
+          <div className="flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+</svg>
 
-    //kddkdkd
-    //   <Header />
-    //   <Layout2 />
-    // </div>
+            <div className="text-gray-500">No Orders</div>
+          </div>
+        )}
+      </div>
+    </div>
+  </Layout>
+</div>
+
+
+   
   );
 }
