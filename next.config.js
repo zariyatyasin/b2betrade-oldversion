748be914f4b1e86b641ff84b0c202ffb018ff7c0
zariyatyasin <64485419+zariyatyasin@users.noreply.github.com>
2024-01-23 12:43:00 +0600

    insha Allah
    Allah is the best

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output:"export",
   images: {
     domains: ["res.cloudinary.com"],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+ },
 };
 
 module.exports = nextConfig;
