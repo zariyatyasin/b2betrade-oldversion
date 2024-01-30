ad579945f25ad0227f6e0e33e74ca204a03a2479
zariyatyasin <64485419+zariyatyasin@users.noreply.github.com>
2024-01-30 13:22:07 +0600

    gg
    gg

--- app/api/cloudinary/route.jsx
+++ app/api/cloudinary/route.jsx
@@ -30,7 +30,7 @@ export const POST = async (request) => {
 
       const bytes = await file.arrayBuffer();
       const buffer = Buffer.from(bytes);
-      const filePath = path.join(process.cwd(), "public/uploads", file.name);
+      const filePath = path.join(process.cwd(), "public/uploadimg", file.name);
 
       await writeFile(filePath, buffer);
 
