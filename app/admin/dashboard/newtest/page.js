b63decf4c5cc7320a7f9e6dccc1a7e0250b22b9b
zariyatyasin <zariyatyasin789@gmail.com>
2024-02-24 12:49:10 +0600

    twsr
    jkj

--- app/admin/dashboard/newtest/page.js
+++ app/admin/dashboard/newtest/page.js
@@ -1,8 +1,8 @@
-import Layout from "../../../../components/admin/Layout/Layout";
-import db from "../../../../utils/db";
 import React from "react";
+
 import Category from "../../../../model/Category";
-import CreateCategories from "../../../../components/admin/categories/CreateCategories";
+import db from "../../../../utils/db";
+import Test from "../../../../components/Test";
 
 async function getData() {
   await db.connectDb();
@@ -20,10 +20,8 @@ export default async function page() {
   const { categories } = await getData();
 
   return (
-    <Layout>
-      <div className="px-4 sm:px-6 lg:px-8">
-        <CreateCategories categories={categories} />
-      </div>
-    </Layout>
+    <div>
+      <Test categories={categories} />
+    </div>
   );
 }
